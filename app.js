0628934182060b794ae4465bc19859d95f01b3f0
unknown <ellenevellyn988@gmail.com>
2024-11-30 18:42:59 -0300

    updade

--- app.js
+++ app.js
@@ -20,3 +20,42 @@ const authRouters = require('./routes/authRoutes');
 
 //Integrando as Rotas ao App
 app.use('/api/auth', authRouters)
+
+// adicionando um endpoint de cadastro 
+
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+
+// simulaçao de um banco de dados (substituir por um banco real mysql)
+let users = [];
+
+ // endpoint de cadastro
+ app.post('/api/auth/register', (req, res) => {
+    const { name, email, password, confirmPassword } = req.body;
+
+    // validaçao
+    if (!name || !email || !password || !confirmPassword) {
+        return res.status(400).json({ message: 'Todos os campos são obrigatórios.'});
+    }
+    if (password !== confirmPassword) {
+        return res.status(400).json({ message: 'As senhas não coincidem.'});
+    }
+
+    // verificar se o email já está cadastrado
+    const existingUser = users.find(user.email === email);
+    if (existingUser) {
+        return res.status(400).json({ message: 'E-mail já cadastrado.'});
+    }
+
+    // salvar o novo usuário
+    user.push({ name, email, password});
+    res.status(201).json({message: 'Usuário cadstrado com sucesso.'});
+    });
+
+// inicializaçao do servidor
+app.listen(5000, () => {
+    console.log('Servidor rodaando em https://localhost:5000');
+});
